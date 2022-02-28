23524820226fc7f97f2ca857a72dc84a53f03c4d
Emilie L'Haridon <emiliemarie31@gmail.com>
2022-02-28 15:03:47 +0100

    First draft of questionCard

---
+++ front/src/components/QuestionCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="question-card">
+    <h4>Vrai ou faux?</h4>
+    <p>{{question.question}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionCard',
+
+  data() {  
+    return {
+      question: {
+        question_id: 1,
+        theme_id: 1,
+        question: "This is a question from the data.",
+        comment: "This is the comment that apears when you validate",
+        answer: true
+      }
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuestionCard/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import QuestionCard from '@/components/QuestionCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    QuestionCard
   }
 }
 </script>
