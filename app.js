f7ecc4303598b7d5b74335a1bc76fc75f3f19d3a
Emilie_Lharidon <emiliemarie31@gmail.com>
2022-03-04 11:49:17 +0100

    Add Cors policy

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+var cors = require('cors');
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 //Database connection
