d3ceaa962a69b704d290c6e490e1f1ab3e5f5807
Emilie L'Haridon <emiliemarie31@gmail.com>
2022-03-21 11:35:00 +0100

    console correction

--- _front/src/components/QuestionCard.vue
+++ _front/src/components/QuestionCard.vue
@@ -64,7 +64,7 @@ export default {
   //Boolean parameters thats changes each time you go to next question in order to change content
   watch: {
     checkPageTurn(newVal, oldVal) {
-      console.log('Prop reset change form', oldVal, 'to', newVal);
+      // console.log('Prop reset change form', oldVal, 'to', newVal);
       this.showComment = false;
       this.correctAnswer = undefined;
     }
