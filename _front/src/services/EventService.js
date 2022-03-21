9f988c81f936771b6901dfd120fab1d311759f28
Emilie L'Haridon <emiliemarie31@gmail.com>
2022-03-21 11:29:10 +0100

    Change of url

--- _front/src/services/EventService.js
+++ _front/src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://quiz-geek.herokuapp.com//api/'
+    baseURL: 'https://quiz-geek.herokuapp.com/api/'
 })
 
 export function getQuestions(theme_id) {
